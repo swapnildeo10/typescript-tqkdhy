11847776b2fdb63129bd949d525e72166fab889e
Swapnil Deo <swapnildeo10@gmail.com>
2022-05-04 22:28:52 +0530

    new test2 changes
    this is test so ignore

--- index.ts
+++ index.ts
@@ -4,6 +4,6 @@ import { Game } from "./game";
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById("app");
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>TypeScript Starter Project So please ignore</h1>`;
 
 new Game(640, 480);
